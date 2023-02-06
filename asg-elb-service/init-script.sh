b1bb683f0070b6d375c4c2c4be23083242ce2b71
KoldanJS <koldan.js@gmail.com>
2023-02-06 19:43:29 +0300

    deleted extra files/code

---
+++ asg-elb-service/init-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install httpd
+
+myip=$(curl -s https://api.ipify.org)
+
+sudo cat <<EOF > /var/www/html/index.html
+<html>
+<body bgcolor="black">
+<h2>
+<font color="gold">Build by Power of Terraform </font><font color="red"> v1.3.*</font>
+<br>
+<font color="green">Server PrivateIP: <font color="aqua">$myip</font></font>
+<br>
+<font color="magenta"><b>Version 0.2.0</b></font>
+</h2>
+</body>
+</html>
+EOF
+
+sudo service httpd start
+sudo chkconfig httpd on
--- asg-elb-service/outputs.tf
+++ asg-elb-service/outputs.tf
@@ -1,19 +1,11 @@
-output "url" {
-  value = "http://${aws_elb.webserver_example.dns_name}:${var.elb_port}"
+output "domain-name" {
+  value = aws_instance.web.public_dns
 }
 
-output "elb_dns_name" {
-  value = aws_elb.webserver_example.dns_name
+output "application-url" {
+  value = "${aws_instance.web.public_dns}/index.php"
 }
 
-output "asg_name" {
-  value = aws_autoscaling_group.webserver_example.name
-}
-
-output "asg_security_group_id" {
-  value = aws_security_group.asg.id
-}
-
-output "elb_security_group_id" {
-  value = aws_security_group.elb.id
+output "public_ip" {
+  value = aws_eip.web_ip.public_ip
 }
